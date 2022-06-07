919550966c75271253c1cd9119abbe300be80dfd
wol4aravio <panovskiy.v@yandex.ru>
2022-06-07 10:09:31 +0300

    test RS

---
+++ tests/opta/algorithms/test_random_search.py
@@ -0,0 +1,16 @@
+import pytest
+
+from opta.algorithms.random_search import RS
+from opta.tools.testing import smoke_check
+
+
+@pytest.mark.parametrize("_", range(10))
+def test_smoke(_):
+    algorithm = RS(1e-1)
+    assert smoke_check(algorithm, number_of_iterations=10_000)
+    try:
+        state = algorithm.serialize()
+        algorithm.deserialize(state)
+        assert True
+    except Exception:
+        assert False
