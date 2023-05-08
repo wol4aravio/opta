10138afb743da8fee7bdc33514c042383eeb23a1
wol4aravio <panovskiy.v@yandex.ru>
2023-05-08 13:11:00 +0300

    docs for testing tools

--- opta/tools/testing.py
+++ opta/tools/testing.py
@@ -2,6 +2,12 @@ import numpy as np
 
 
 def smoke_check(algorithm, number_of_iterations, eps=1e-2):
+    """
+    Smoke check for optimization tasks:
+    `algorithm` instance's performance is checked on quadratic function
+    with randomly generated single minima.
+    Algorithms runs `number_of_iterations` and target error must satisfy
+    accuracy conditin defined via `eps`"""
     left, right = -10, 10
     search_area = np.array([[left, right], [left, right]])
     x_opt = np.random.uniform(left, right, 2)
