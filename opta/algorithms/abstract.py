b1ac5315d4f6d8cb9798e194dd6b35ec35182a86
wol4aravio <panovskiy.v@yandex.ru>
2023-05-08 15:25:14 +0300

    docs for abstract class

--- opta/algorithms/abstract.py
+++ opta/algorithms/abstract.py
@@ -5,29 +5,42 @@ from opta.tools.coding import DecodeToNumpy, EncodeFromNumpy
 
 
 class TerminationException(Exception):
+    """Exception that is used to terminate algorithm"""
+
     pass
 
 
 class OptimizationAlgorithm(ABC):
+    """Class that should be inherited by optimization algorithms"""
+
     @abstractmethod
     def initialize(self, f, search_area):
+        """Initialization procedure based on target function `f` and `search_area`"""
         raise NotImplementedError()
 
     @abstractmethod
     def iterate(self, f, search_area):
+        """Iteration procedure based on target function `f` and `search_area`"""
         raise NotImplementedError()
 
     @abstractmethod
     def terminate(self, f, search_area):
+        """Iteration procedure based on target function `f` and `search_area`"""
         raise NotImplementedError()
 
     def serialize(self):
+        """Serialization procedure"""
         return json.dumps(self.__dict__, indent=4, cls=EncodeFromNumpy)
 
     def deserialize(self, state):
+        """Deserialization procedure"""
         self.__dict__ = json.loads(state, cls=DecodeToNumpy)
 
     def optimize(self, f, search_area, number_of_iterations, **kwargs):
+        """Optimization cycle of target function `f` on `search_area`
+        that consists of initialization, followed by repeating cycle of
+        initialization procedures (until termination criterion is fullfilled),
+        and termination"""
         if not kwargs.get("skip_init", False):
             self.initialize(f, search_area)
         for _ in range(number_of_iterations):
