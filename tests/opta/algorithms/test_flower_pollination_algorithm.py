7a6bfb99a2fcaf9ab49d9cdf7d16a5c3383c8f03
wol4aravio <panovskiy.v@yandex.ru>
2023-05-12 09:40:48 +0300

    fixed tetst + added FPA

--- tests/opta/algorithms/test_flower_pollination_algorithm.py
+++ tests/opta/algorithms/test_flower_pollination_algorithm.py
@@ -1,13 +1,13 @@
 import pytest
 
-from opta.algorithms.black_hole_optimization import BHO
+from opta.algorithms.flower_pollination_algorithm import FPA
 from opta.tools.testing import smoke_check
 
 
 @pytest.mark.parametrize("_", range(25))
 def test_smoke(_):
-    algorithm = BHO(100)
-    assert smoke_check(algorithm, number_of_iterations=1_000)
+    algorithm = FPA(25, 0.9, 0.25, 1.25)
+    assert smoke_check(algorithm, number_of_iterations=10_000)
     try:
         state = algorithm.serialize()
         algorithm.deserialize(state)
